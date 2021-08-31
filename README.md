1da35aec825ec8b32439dded557760fe9e6c018b
mkonemo <mkonemo@gmail.com>
2021-08-31 16:46:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-btamtv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-btamtv)
---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ExpenseItem from './components/ExpenseItem';
+
+import './Expenses.css';
+
+function Expenses(props) {
+  return (
+    <div>
+      <p>Content</p>
+    </div>
+  );
+}
+
+export default Expenses;
